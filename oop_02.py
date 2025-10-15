3cdd5529b61a03d1299b11a6d97a388e53f3c352
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-10-15 19:17:43 +0200

    Update oop_02.py

--- oop_02.py
+++ oop_02.py
@@ -1,19 +1,40 @@
-# naziv, broj nogu, boja, glasanje je funkcionalnost
-
-
 class Pet:
-    pass
+    def __init__(self,
+                 name,
+                 number_of_legs,
+                 colour):
+        self.name = name
+        self.number_of_legs = number_of_legs
+        self.colour = colour
+
+    def make_sound(self):
+        return ''
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Cat(Pet):
+    def __init__(self, name, number_of_legs, colour, loves_mices = True):
+        super().__init__(name, number_of_legs, colour)
+        self.loves_mices = loves_mices
+
+    def make_sound(self):
+        return 'Miau, miau'
+
+
+class Dog(Pet):
+    def __init__(self, name, number_of_legs, colour, loves_bones = True):
+        super().__init__(name, number_of_legs, colour)
+        self.loves_bones = loves_bones
+
+    def make_sound(self):
+        return 'Vau, vau'
 
 
 
-class Cat:
-    pass
+cat = Cat('Tom', 4, 'Blue-White')
+dog = Dog('Goofy', 4, 'Browne', False)
 
-
-class Dog:
-    pass
-
-
-
-cat = Cat()
-dog = Dog()
+print(cat.make_sound())
+print(dog.make_sound())
